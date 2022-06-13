8c9e177c5c9fb74febf45eaf06dd1a7057ddc8d0
gibbonhug <71813760+gibbonhug@users.noreply.github.com>
2022-06-13 18:02:50 -0400

    Add inline documentation for Book component

--- src/Book.tsx
+++ src/Book.tsx
@@ -8,6 +8,14 @@ interface BookProps {
   read: boolean;
 }
 
+
+/**
+ * A book
+ * Title: The title of the book
+ * Author: The author of the book
+ * pageCount: The number of pages of the book
+ * read: Whether the book has been read or not
+ */
 export default function Book({title, author, pageCount, read}: BookProps ) {
   return (
     <div>
