f5d6e4c90b8bdb9a6bdbea230b7d1f9bc19c4466
gibbonhug <71813760+gibbonhug@users.noreply.github.com>
2022-06-13 17:09:23 -0400

    Change type of library to array of books

--- source.js
+++ source.js
@@ -1,3 +1,6 @@
 "use strict";
 console.log('hello typescript');
 const myLibrary = [];
+function Book(title, author, pageCount, readStatus) {
+    this.title = title;
+}
--- source.ts
+++ source.ts
@@ -1,3 +1,33 @@
 console.log('hello typescript');
 
-const myLibrary: string[] = [];
+const myLibrary: Book[] = [];
+
+type Book = {
+    /**
+     * The title of the book
+     */
+    title: string;
+    /**
+     * The author of the book (First Last)
+     */
+    author: string;
+    /**
+     * How many pages the book has
+     */
+    pageCount: number;
+    /**
+     * Whether the book is read or unread
+     */
+    readStatus: boolean;
+}
+
+function Book(
+    this: Book,
+    title: string, 
+    author: string, 
+    pageCount: number,
+    readStatus: boolean
+    ) {
+    this.title = title;
+}
+
